b8db3f2d72d312dd2fddaddaa3dbbda327c4bda9
iamrakibsarder <iamrakibsarderofficial@gmail.com>
2021-12-18 00:09:41 +0600

    My Bank

---
+++ function.js
@@ -0,0 +1,51 @@
+const clickEnter = document.getElementById('btnSubmit');
+        clickEnter.addEventListener('click', function(){
+            var login = document.getElementById('login-page');
+            login.style.display = 'none';
+            var dashboard = document.getElementById('dashboard');
+            dashboard.style.display = 'block';
+
+        })
+
+
+        // Deposit Section JS
+
+        const addingDeposit = document.getElementById('btnDeposit');
+        addingDeposit.addEventListener('click', function(){
+
+            const getInputNumber = inputAmount('deposit-amount');
+
+            addAmount('deposit-dollar', getInputNumber);
+            addAmount('balance', getInputNumber);
+
+            document.getElementById('deposit-amount').value = '';
+
+        })
+
+        // Withdraw Section JS 
+
+        const doingWithdraw = document.getElementById('btnWithdraw');
+        doingWithdraw.addEventListener('click', function(){
+            const getInputNumber = inputAmount('withdraw-amount');
+            addAmount('withdraw-dollar', getInputNumber);
+            addAmount('balance', -1 * getInputNumber);
+
+            document.getElementById('withdraw-amount').value = '';
+        })
+
+
+        // Getting Input Function 
+
+        function inputAmount(id){
+                const inputMoney = document.getElementById(id).value;
+                const inputMoneyNumber = parseFloat(inputMoney);
+                return inputMoneyNumber;
+            }
+
+            // Addning Amount Function 
+        function addAmount(id, getInputNumber){
+                const current = document.getElementById(id).innerText;
+                const currentNum = parseFloat(current);
+                const totalAmountAdd = currentNum + getInputNumber;
+                document.getElementById(id).innerText = totalAmountAdd;
+            }
